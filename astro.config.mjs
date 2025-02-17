05ffdabace7a25d6c087fec29c1fa301e952a533
JP Hutchins <jphutchins@gmail.com>
2025-02-16 17:45:52 -0800

    wip blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://jphutchins.github.io",
-    base: "interstellar-inclination",
 });
